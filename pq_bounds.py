b22d18eeca82a1f20613708896d954f3090e37ea
Fei Wei <weifei086@gmail.com>
2022-06-23 18:56:56 -0700

    Updates

--- pq_bounds.py
+++ pq_bounds.py
@@ -5,6 +5,7 @@ from scipy.spatial import ConvexHull
 def pq_upper(Eg,gamma_grid):
     
     Eg_grid = [Eg(gamma) for gamma in gamma_grid]
+    Eg_grid = np.array(Eg_grid)
     
     # We are ready to compute the new (p3,q3) pairs, which is unifom on log(gamma)
     aa = (Eg_grid[2:-1]-Eg_grid[1:-2])/(gamma_grid[2:-1]-gamma_grid[1:-2])
@@ -12,10 +13,10 @@ def pq_upper(Eg,gamma_grid):
     
     init_slope = (Eg_grid[0]-1)/gamma_grid[0]
     q = aa-bb
-    q = [1+init_slope,bb[0]-init_slope,q2,-aa[-1]];
+    q = [1+init_slope,bb[0]-init_slope,q,-aa[-1]];
     p = [0,q[1:]*gamma_grid]
     q = [q,0]
-    p = [p,max(0,1-sum(p))]
+    p = [p,np.maximum(0,1-sum(p))]
     return p,q
     
 def pq_lower(Eg,Egslope,gamma_grid):
@@ -39,9 +40,15 @@ def pq_lower(Eg,Egslope,gamma_grid):
                 slp = (Eg(t)-f_grid[i-1])/(t-gamma_grid[i-1])
             f_grid[i] = max(0,f_grid[i-1]+slp*(gamma_grid[i]-gamma_grid[i-1]))
     gamma_grid_ext = [0,gamma_grid]
+    print(np.shape(gamma_grid_ext))
     f_grid_ext = [1,f_grid]
-    K = ConvexHull([gamma_grid_ext;f_grid_ext])
+    hull = ConvexHull([gamma_grid_ext,f_grid_ext])
     
+    K=hull.simplices
     slope = ([f_grid_ext[i] for i in K[1:-1]]-[f_grid_ext[i] for i in K[0:-2]])/([gamma_grid_ext[i] for i in K[1:-1]]-[gamma_grid_ext[i] for i in K[0:-2]])
     q = np.zeros(np.shape(gamma_grid))
-    for 
+    for j in range(1,len(K)-2):
+        q[K[j]] = slope[j]-slope[j-1]
+    q[-1]=-slope[-2]
+    p = q*gamma_grid
+    return p,q
